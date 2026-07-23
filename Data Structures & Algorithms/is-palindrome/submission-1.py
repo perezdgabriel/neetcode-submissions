069e9ef563cd959b541c11bf7891ec967f4517f4
Gabriel Perez <perezd.gabriel@gmail.com>
2026-07-23 19:02:59 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,12 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        word = re.sub(r"[^a-zA-Z0-9]", "", s).lower()
+        l, r = 0, len(word) - 1
+        while l < r:
+            if word[l] != word[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
