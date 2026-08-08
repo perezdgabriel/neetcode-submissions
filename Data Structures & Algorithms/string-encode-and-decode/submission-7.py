61334f51d6ff69ed9059352cbd9caa06c684bf2e
Gabriel Perez <perezd.gabriel@gmail.com>
2026-08-07 20:32:15 -0400

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.py
@@ -0,0 +1,25 @@
+class Solution:
+    len_delimiter = '#'
+
+    def encode(self, strs: List[str]) -> str:
+        res = ''
+        for s in strs:
+            res += str(len(s)) + self.len_delimiter + s
+
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        while i < len(s):
+            len_str = ''
+            while s[i] != self.len_delimiter:
+                len_str += s[i]
+                i += 1
+            i += 1
+            length = int(len_str)
+            res.append(s[i: i + length])
+            i += length
+        return res
+
+        
